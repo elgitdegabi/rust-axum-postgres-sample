97332abbb474dade321560f139f06ccbc9a00a57
Gabi <gabelopment@gmail.com>
2023-05-01 20:13:17 -0300

    JIRA-NNNN - rust axum postgres api sample - add initial implementation

---
+++ src/controller/user_controller.rs
@@ -0,0 +1,74 @@
+use axum::{
+    extract::Path,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde_json::json;
+
+use crate::config::constants::*;
+use crate::model::health::Health;
+use crate::model::user::UserUpsert;
+use crate::service::user_service::*;
+
+pub fn config_endpoints() -> Router {
+    async fn map_health() -> impl IntoResponse {
+        format!(
+            "{}",
+            json!(Health {
+                status: String::from(SERVER_RUNNING_STATUS)
+            })
+        )
+    }
+
+    async fn map_get_users() -> impl IntoResponse {
+        format!("{}", json!(get_users()))
+    }
+
+    async fn map_get_user(Path(user_id): Path<i64>) -> impl IntoResponse {
+        format!("{}", json!(get_user(user_id)))
+    }
+
+    async fn map_add_user(user: Json<UserUpsert>) -> impl IntoResponse {
+        format!("{}", json!(add_user(user.0)))
+    }
+
+    async fn map_update_user(
+        Path(user_id): Path<i64>,
+        user: Json<UserUpsert>,
+    ) -> impl IntoResponse {
+        format!("{}", json!(update_user(user_id, user.0)))
+    }
+
+    async fn map_delete_user(Path(user_id): Path<i64>) -> impl IntoResponse {
+        format!("{}", json!(delete_user(user_id)))
+    }
+
+    Router::new()
+        .route("/health", get(map_health))
+        .route("/users", get(map_get_users))
+        .route("/user/:userid", get(map_get_user))
+        .route("/user/add", post(map_add_user))
+        .route("/user/update/:userid", post(map_update_user))
+        .route("/user/delete/:userid", post(map_delete_user))
+}
+
+/**
+ * Unit test cases
+ */
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /**
+     * Scenario:
+     * Executes config_endpoints with valid API server
+     * Expectation:
+     * A set of endpoints should be created
+     */
+    #[test]
+    fn when_config_endpoints_with_api_server_should_add_endpoints() {
+        // TODO to be implemented
+        assert_eq!(true, true);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use crate::config::constants::HOST;
+use crate::controller::user_controller::config_endpoints;
+
+use config::database::*;
+use dotenv::dotenv;
+use log::info;
+use std::env;
+
+mod config;
+mod controller;
+mod model;
+mod repository;
+mod schema;
+mod service;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let log_config_file =
+        env::var("LOG4RS_CONFIG_FILE").unwrap_or("logging_config.yaml".to_string());
+    log4rs::init_file(log_config_file, Default::default()).unwrap();
+
+    unsafe {
+        info!("DB pool - starting...");
+        DB_POOL = Some(create_db_pool().await);
+        info!("DB pool - started OK: {:?}", DB_POOL.as_ref().unwrap());
+    }
+
+    info!("Axum server - starting...");
+    axum::Server::bind(&HOST.parse().unwrap())
+        .serve(config_endpoints().into_make_service())
+        .await
+        .unwrap();
+}
